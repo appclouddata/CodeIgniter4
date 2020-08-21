e55703f9b1b7d080f1e9eb485b1051ce2f733426
AppCloudData <robin.appclouddata@gmail.com>
2020-08-21 19:15:24 +0530

    Update CodeIgniter.php
    [20-Aug-2020 23:13:08 America/New_York] PHP Fatal error:  Uncaught Error: Call to undefined function CodeIgniter\locale_set_default() in /home/AppCloudData/public_html/system/CodeIgniter.php:184
    Stack trace:
    #0 /home/AppCloudData/public_html/system/bootstrap.php(181): CodeIgniter\CodeIgniter->initialize()
    #1 /home/AppCloudData/public_html/public/index.php(36): require('/home/AppCloudData/...')
    #2 {main}
      thrown in /home/AppCloudData/public_html/system/CodeIgniter.php on line 184

--- system/CodeIgniter.php
+++ system/CodeIgniter.php
@@ -198,7 +198,11 @@ class CodeIgniter
 		}
 
 		// Set default locale on the server
-		locale_set_default($this->config->defaultLocale ?? 'en');
+		// @AppCloudData ADDED FOLLOWING CHECK TO ON undefined function ERROR
+		if( function_exists('locale_set_default' ) ) :
+		    locale_set_default($this->config->defaultLocale ?? 'en');
+		endif; 
+		
 
 		// Set default timezone on the server
 		date_default_timezone_set($this->config->appTimezone ?? 'UTC');
